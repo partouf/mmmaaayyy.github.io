13cdf2a64e5dfaa7955eb1de61093bcbb7c9c4fa
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-11 13:06:13 -0400

    ,fvbnljgwb

--- file.js
+++ file.js
@@ -1,10 +1,9 @@
 .stuff{
 <input type="button" onclick="location.href='http://google.com';" value="Go to Google" />
 }
-
-void setup Screen ()
+window.print() 
 {
 	size(600,600);
-	background (0,0,0);
+	background (66,245,34);
 
 }
