073567965d602aa5af86283d5d7af8d4f27292d4
mmmaaayyy <mmaayy09@gmail.com>
2015-08-11 22:24:15 -0400

    fd

--- file.js
+++ file.js
@@ -1,8 +1,7 @@
-//object.onclick=function(){myScript};
-//document.getElementById("demo").addEventListener("click", myFunction);
 
 function pictureChange()
 {
 document.getElementById(theImage).src="bedroom.png");
 }
 
+pictureChange();
