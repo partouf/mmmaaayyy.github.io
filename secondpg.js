57d0ee3cc5072247308d386f4d3bc0f1aad2dd4e
mmmaaayyy <mmaayy09@gmail.com>
2015-08-12 14:45:51 -0400

    fdsf

--- secondpg.js
+++ secondpg.js
@@ -28,10 +28,10 @@ var max=0;
 
 
 
+document.getElementById('button').onclick = function() {
+#para1 {
+<p>You are a 16-year-old girl who is a junior at North West High School. You love singing, playing sports, hanging out with your friends, and learning new things. This is an important year for you to focus on your academics, and you’re starting to think you want to be an engineer! This is a typical day in your life.</p> = <p>testing fr changes</p>;
+alert("please work");
+}
+}
 
-
-
-
-
-//document.getElementById('button').onclick = function() {
-//text="Makeup";
