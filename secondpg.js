664f52573055ac71d0fb3a66e200bfd82980bf07
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-11 22:25:07 -0400

    working on self type function

--- secondpg.js
+++ secondpg.js
@@ -1,16 +1,18 @@
-var text = "OMG you woke up late what do you do?!?";
-var charCount = text.length;
-var currentLetterCount = 0;
-var speed = 100; // How fast should it type?
-var $input = document.getElementById("someInput");
+var timer = 0;
 
-function writeLetter() {
-    var currentText = $input.value;
-    var currentLetter = text.charAt(currentLetterCount);
-    currentLetterCount++;
-    $input.value = currentText + currentLetter;
-    if(currentLetterCount == charCount)
-        clearInterval(timerId);
+timer = setInterval ( "updateElement()", 5000 );
+
+function updateElement ( )
+{
+  var stringToType = "this text will be typed you woke up late and you have to either put on makeup or get dressed,girl";
+  var oldValue = getElementById('myelement').Value;
+
+if(stringToType == oldValue)
+{
+  clearInterval(timer);
+}  
+else
+{
+  getElementById('myelement').Value = oldValue + stringToType.subString(oldValue.length, 1);
 }
-
-var timerId = setInterval(writeLetter, speed);
+}
