5c27acbb34ba79f0a66260af935e7b743b731902
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-12 11:02:34 -0400

    vs

--- secondpg.js
+++ secondpg.js
@@ -21,7 +21,7 @@ var max=0;
       function textticker()
       {
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
+        if(pos++==l) { pos=0; setTimeout("textticker()",100); x++;
         if(x==max) x=0; l=tl[x].length; } else
         setTimeout("textticker()",50);
       }
