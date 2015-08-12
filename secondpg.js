5e8daed00e0fcc9331080831778a0db966936f53
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-12 10:30:53 -0400

    etwg3

--- secondpg.js
+++ secondpg.js
@@ -1,19 +1,30 @@
 
-var text = "The quick fox jumped over the lazy dog.";
-var charCount = text.length;
-var currentLetterCount = 0;
-var speed = 100; 
-var $input = $(text);
+var max=0;
+      function textlist()
+      {
+      max=textlist.arguments.length;
+      for (i=0; i<max; i++)
+      this[i]=textlist.arguments[i];
+      }
+      tl=new textlist
+      ( 
+       "This is a  message",    
+       "Another one",
+       "And this will be the third",
+       "And the fourth is the last !"
 
-function writeLetter() {
-    var currentText = $input.val();
-    var currentLetter = text.charAt(currentLetterCount);
-    currentLetterCount++;
-    $input.val(currentText + currentLetter);
-    if(currentLetterCount == charCount)
-        clearInterval(timerId);
-}
-var timerId = setInterval(writeLetter,speed);
+
+
+      );
+      var x=0; pos=0;
+      var l=tl[0].length;
+      function textticker()
+      {
+        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
+        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
+        if(x==max) x=0; l=tl[x].length; } else
+        setTimeout("textticker()",50);
+      }
 
 
 function myFunction(){
