aebb9e101cf1c6ebef6696c246f1b60fd1648f32
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-13 11:25:44 -0400

    kj

--- secondpg.js
+++ secondpg.js
@@ -14,23 +14,8 @@ var max=0;
        "And the fourth is the last !"
        
       );
-<<<<<<< HEAD
 
       var x=0; pos=0;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      var x=0; pos=0;
-=======
-=======
->>>>>>> origin/master
->>>>>>> origin/master
-<<<<<<< HEAD
-      var x=0; pos=50;
->>>>>>> origin/master
->>>>>>> origin/master
       var l=tl[0].length;
       function textticker()
       {
@@ -44,21 +29,7 @@ var max=0;
       {
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
         if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-<<<<<<< HEAD
         if(x==max) x=0; l=tl[x].length; } else
-=======
-        if(x==max) x=60; l=tl[x].length; } else
->>>>>>> parent of ca16575... Revert "dsv"
-=======
-      var x=60; pos=0;
-      var l=tl[60].length;
-      function textticker()
-      {
-        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-        if(x==max) x=60; l=tl[x].length; } else
->>>>>>> parent of ca16575... Revert "dsv"
->>>>>>> origin/master
         setTimeout("textticker()",50);
       }
 
