0e84a654110bdd8e52668e21787be53c10c7a302
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-13 11:29:33 -0400

    sdvc

--- secondpg.js
+++ secondpg.js
@@ -15,78 +15,14 @@ var max=0;
        
       );
 
-=======
-<<<<<<< HEAD
-
-      var x=0; pos=0;
-<<<<<<< HEAD
-=======
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> origin/master
       var x=0; pos=0;
 
-      var x=0; pos=50;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
       var l=tl[0].length;
       function textticker()
       {
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
         if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
         if(x==max) x=0; l=tl[x].length; } else
-
-<<<<<<< HEAD
-      var x=60; pos=0;
-      var l=tl[60].length;
-=======
-      var x=0; pos=0;
-      var l=tl[0].length;
->>>>>>> origin/master
-      function textticker()
-      {
-        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-<<<<<<< HEAD
-        if(x==max) x=0; l=tl[x].length; } else
-=======
-<<<<<<< HEAD
-        if(x==max) x=60; l=tl[x].length; } else
-
-      var x=60; pos=0;
-      var l=tl[60].length;
-      function textticker()
-      {
-        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-=======
-<<<<<<< HEAD
-        if(x==max) x=0; l=tl[x].length; } else
-=======
->>>>>>> origin/master
-        if(x==max) x=60; l=tl[x].length; } else
-
-      var x=60; pos=0;
-      var l=tl[60].length;
-      function textticker()
-      {
-        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-        if(x==max) x=60; l=tl[x].length; } else
-<<<<<<< HEAD
-=======
->>>>>>> parent of ca16575... Revert "dsv"
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
         setTimeout("textticker()",50);
       }
 
