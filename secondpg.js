667aff3c89b1d071cc7e451ff30a221a65c1174c
mmmaaayyy <mmaayy09@gmail.com>
2015-08-13 11:21:44 -0400

    kjn

--- secondpg.js
+++ secondpg.js
@@ -14,6 +14,7 @@ var max=0;
        "And the fourth is the last !"
        
       );
+<<<<<<< HEAD
       var x=0; pos=50;
       var l=tl[0].length;
       function textticker()
@@ -21,6 +22,15 @@ var max=0;
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
         if(pos++==l) { pos=50; setTimeout("textticker()",1000); x++;
         if(x==max) x=0; l=tl[x].length; } else
+=======
+      var x=60; pos=0;
+      var l=tl[60].length;
+      function textticker()
+      {
+        document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
+        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
+        if(x==max) x=60; l=tl[x].length; } else
+>>>>>>> parent of 24898dc... grw
         setTimeout("textticker()",50);
       }
 
