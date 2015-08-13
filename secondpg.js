3a1e9b7194e52c0eb34f8808047a53dba20ea047
mmmaaayyy <mmaayy09@gmail.com>
2015-08-13 13:19:43 -0400

    picture

--- secondpg.js
+++ secondpg.js
@@ -33,4 +33,5 @@ var max=0;
  function showhide(id) {
     var e = document.getElementById(id);
     e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+    document.write("<img src='screenshot_1.png' border=0>");
     }
