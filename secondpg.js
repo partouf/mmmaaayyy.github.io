24898dca9b929f11784c6b2172fa9cd81dc59162
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-13 11:13:13 -0400

    grw

--- secondpg.js
+++ secondpg.js
@@ -14,13 +14,13 @@ var max=0;
        "And the fourth is the last !"
        
       );
-      var x=60; pos=0;
-      var l=tl[60].length;
+      var x=0; pos=0;
+      var l=tl[0].length;
       function textticker()
       {
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
         if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
-        if(x==max) x=60; l=tl[x].length; } else
+        if(x==max) x=0; l=tl[x].length; } else
         setTimeout("textticker()",50);
       }
 
