965434082cae34c31b7e0720b0dd7c4d5e082211
mmmaaayyy <mmaayy09@gmail.com>
2015-08-13 09:38:54 -0400

    sfd

--- secondpg.js
+++ secondpg.js
@@ -29,3 +29,11 @@ var max=0;
 //}
 
 //myFunction();
+
+function toggle_visibility() {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+   }
