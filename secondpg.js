84619f9ebd2ee6907cc0dd7235b5f6f7372371eb
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-13 11:21:44 -0400

    Revert 24898dc..ca16575
    This rolls back to commit 24898dca9b929f11784c6b2172fa9cd81dc59162.

--- secondpg.js
+++ secondpg.js
@@ -14,12 +14,12 @@ var max=0;
        "And the fourth is the last !"
        
       );
-      var x=0; pos=50;
+      var x=0; pos=0;
       var l=tl[0].length;
       function textticker()
       {
         document.tickform.tickfield.value=tl[x].substring(0,pos)+"_";
-        if(pos++==l) { pos=50; setTimeout("textticker()",1000); x++;
+        if(pos++==l) { pos=0; setTimeout("textticker()",1000); x++;
         if(x==max) x=0; l=tl[x].length; } else
         setTimeout("textticker()",50);
       }
