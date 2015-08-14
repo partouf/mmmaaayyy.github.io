ec5c57b8bf1ed8e1f52d5d65fcea38ad5204d6f8
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-14 11:39:56 -0400

    UITIKGJF

--- mappg.js
+++ mappg.js
@@ -2,42 +2,67 @@
 // Look things up in the documentation and tell me what everything does!
 // *********************************************************************
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiamVmZnN0ZXJuIiwiYSI6IlAzRFFiN0EifQ.mNWvayrLEw9wULuq0sopyA';
-var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([38.12367, -76.81229], 9);
+L.mapbox.accessToken = 'pk.eyJ1IjoiamVmZnN0ZXJuIiwiYSI6IlAzRFFiN0EifQ.mNWvayrLEw9wULuq0sopyA'; // allows us to use the API
+var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([38.12367, -76.81229], 9); // initial position ([latitude, longitude], zoom))
 
-var myLayer = L.mapbox.featureLayer().addTo(map);
+var myLayer = L.mapbox.featureLayer().addTo(map); //allows us to use GeoJson
 
 var geojson = {
     type: 'FeatureCollection',
 
-
     // This is an array of Map Point objects
     features: [
-	
-	
-	
+    
     {
         type: 'Feature',
-        properties: {
-            title: 'Philadelphia, PA',
-			description: 'Fresh Prince of Bel Air <img src="http://www.wallpaperup.com/uploads/wallpapers/2014/04/16/334587/big_thumb_8bdfbfd19dae55e8dd6c39910015b66d.jpg"class="popupimage" /> ',
-            'marker-color': '#66ff00',
+        properties: { //describes title, description
+            title: 'Bay Area, CA',
+            description: 'There are FIFTEEN summer immersion programs in the Bay Area this year!',
+            'marker-color': '#f9d62e',
             'marker-size': 'large',
-            'marker-symbol': 'garden',
+            'marker-symbol': 'marker',
         },
-        geometry: {
+        geometry: { //the marker is a point (also determines the location)
             type: 'Point',
-            coordinates: [-75.17, 39.9]
+            coordinates: [-122.419, 37.774]
         }
     },
     {
         type: 'Feature',
         properties: {
-            title: 'Miami, FL',
-            description: 'Miamiiiiiiiiiiiiiiiiii',
-            'marker-color': '#ffff00',
+            title: 'New York and Newark',
+            description: 'Girls Who Code\'s first program was here in 2012!',
+            'marker-color': '#eae374',
             'marker-size': 'large',
-            'marker-symbol': 'ice-cream',
+            'marker-symbol': 'marker',
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-74.0059, 40.7127837]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            title: 'Seattle, WA',
+            description: 'GWC is in Seattle for the second year!',
+            'marker-color': '#BE9A6B',
+            'marker-size': 'large',
+            'marker-symbol': 'cafe',
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [-122.332, 47.606]
+        }
+    },
+    {
+        type: 'Feature',
+        properties: {
+            title: 'Miami, Florida',
+            description: 'I hope all that sun doesn\'t melt the computers',
+            'marker-color': '#7ec9b1',
+            'marker-size': 'large',
+            'marker-symbol': 'marker',
         },
         geometry: {
             type: 'Point',
@@ -47,53 +72,46 @@ var geojson = {
         {
         type: 'Feature',
         properties: {
-            title: 'Los Angeles, CA',
-            'description': 'Los Angeles: where the stars are!<img src="http://americaninsights.org/wp-content/uploads/2014/06/hollywood.jpg" class="popupimage" /> ',
-            'marker-color': '#330066',
+            title: 'Los Angeles',
+            description: 'In Los Angeles, everyone is a star!<br />-Denzel Washington<br /><img src="http://i.imgur.com/pLQpP1H.png" class="popupimage" /> ',
+            'marker-color': '#fc913a',
             'marker-size': 'large',
-            'marker-symbol': 'cinema',
+            'marker-symbol': 'star',
         },
         geometry: {
             type: 'Point',
             coordinates: [-118.24, 34.05]
         }
     },
+    //add washington DC and Chicao
     {
         type: 'Feature',
         properties: {
-            title: 'Boston, MA',
-            description: 'Yankees and Ivy League <img src="http://www.aede-laweconomics.org/wp-content/uploads/2012/06/harvard-courses1.jpg" class="popupimage" /> ',
-            'marker-color': '#00ccff',
+            title: 'Washington, DC',
+            description: 'Hey, its the whitehouse!',
+            'marker-color': '#9999CC',
             'marker-size': 'large',
-            'marker-symbol': 'industrial',
+            'marker-symbol': 'town-hall',
         },
         geometry: {
             type: 'Point',
-            coordinates: [-71.05, 42.36]
+            coordinates: [-77.03687070000001, 38.9071923]
         }
     },
-
-	    {
+    {
         type: 'Feature',
         properties: {
-            title: 'Las Vegas, NV',
-            description: 'What happens in Vegas stays in Vegas <img src="https://beersnobsquad.files.wordpress.com/2015/05/vegas2.jpg" class="popupimage" /> ',
-            'marker-color': '#6800ff',
+            title: 'Chicago, IL',
+            description: 'Woooooot. I know literally nothing about Chicago!',
+            'marker-color': '#FDE8D7',
             'marker-size': 'large',
-            'marker-symbol': 'star',
+            'marker-symbol': 'dog-park',
         },
         geometry: {
             type: 'Point',
-            coordinates: [-115.14, 36.17]
+            coordinates: [-87.62979819999998, 41.8781136]
         }
     },
-	
-	
-	
-	
-	
-	
-	
     ]
 };
 
