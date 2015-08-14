848584ceb65b82c937b4a88350626c22a19eb43e
maevemiller <maeve.l.miller@gmail.com>
2015-08-13 23:00:19 -0400

    g

--- secondpg.js
+++ secondpg.js
@@ -92,19 +92,6 @@
 	$('#hiddenTextnext').show(); return false;
 }
 
-var myString= "blah blah testing...";
-var myArray= myString.split("");
-var loopTimer:
-function frameLooper () {
-  if(myArray.length >0){
-    document.getElementById("myTypingText").innerHTML+= myArray.shift();
 
-  } else {
-    clearTimeout(loopTimer);
-    return false;
-  }
-  loopTimer= setTimeout('frameLooper()',70);
-}
-frameLooper();
   
     
