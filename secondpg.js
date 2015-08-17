172ef681c5b32d5aba08e03e1e2356da9aad0fb0
maevemiller <maeve.l.miller@gmail.com>
2015-08-16 21:22:46 -0400

    b

--- secondpg.js
+++ secondpg.js
@@ -249,9 +249,9 @@ function functank(id)
 }
 
 
-function funcnext2(id) 
+function funcbaggy(id) 
  {
-  document.getElementById("buttonnext1").style.display = "block";
+  document.getElementById("buttonnext2").style.display = "block";
     var e = document.getElementById(id); 
     if(e.style.display == "none")
     e.style.display = "block";
@@ -287,7 +287,7 @@ function funcnext1(id)
 
 }
 
-function choosebaggy(id)
+function funcnext2(id)
 {
   document.getElementById("buttonfinally2").style.display = "block";
    var e = document.getElementById(id); 
@@ -306,7 +306,7 @@ function choosebaggy(id)
 
  function funcfinally1(id) //when next button is clicked.....
  {
-document.getElementById("buttonnextclass1").style.display = "block";
+document.getElementById("buttonnextclass").style.display = "block";
 var e = document.getElementById(id); 
     if(e.style.display == "none")
     e.style.display = "block";
@@ -318,6 +318,22 @@ var e = document.getElementById(id);
   else
     p.style.display = "none";
   $('#shownotice1').show(); return false;  
+}
+
+function funcfinally2(id) //when next button is clicked.....
+ {
+document.getElementById("buttonnextclass1").style.display = "block";
+var e = document.getElementById(id); 
+    if(e.style.display == "none")
+    e.style.display = "block";
+  else
+    e.style.display = "none";
+    var p = document.getElementById("showfinally2");
+    if(p.style.display == "none")
+    p.style.display = "block";
+  else
+    p.style.display = "none";
+  $('#shownotice2').show(); return false;  
 }
 
  
