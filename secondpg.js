2314f2a539d58f01da8d4daa5aa39ffcbd4cac5b
karlamx <karla@Karlas-MacBook-Pro-2.local>
2015-08-17 16:36:29 -0400

    wev;

--- secondpg.js
+++ secondpg.js
@@ -255,6 +255,7 @@ var p = document.getElementById("chooseclass");
  {
    	document.getElementById("smallscreen").style.backgroundImage = "url('classroom.png')";
  document.getElementById("nextnext1").style.display = "block";
+ document.getElementById("pop2").style.display = "block";
 var e = document.getElementById(id); 
     if(e.style.display == "none")
     e.style.display = "block";
