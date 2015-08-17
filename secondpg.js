883d1ecaad11a7e429efac6cbf7d17ad3b3cc9c9
maevemiller <maeve.l.miller@gmail.com>
2015-08-17 00:16:46 -0400

    q

--- secondpg.js
+++ secondpg.js
@@ -285,3 +285,19 @@ var e = document.getElementById(id);
     p.style.display = "none";
   $('#shownotice').show(); return false;  
 }
+
+ function funcfinally1(id) //when next button is clicked.....
+ {
+document.getElementById("buttonnextclass").style.display = "block";
+var e = document.getElementById(id); 
+    if(e.style.display == "none")
+    e.style.display = "block";
+  else
+    e.style.display = "none";
+    var p = document.getElementById("showfinally1");
+    if(p.style.display == "none")
+    p.style.display = "block";
+  else
+    p.style.display = "none";
+  $('#shownotice1').show(); return false;  
+}
